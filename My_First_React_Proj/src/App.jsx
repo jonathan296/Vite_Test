7ef67da2e0e8cf5a01d9c0e27ec8e232321fef43
jonathan296 <jonathanalvarenga296@gmail.com>
2023-10-02 00:14:31 -0700

    added data js file

--- My_First_React_Proj/src/App.jsx
+++ My_First_React_Proj/src/App.jsx
@@ -1,14 +1,20 @@
-
+import { useState } from "react";
+import { puppyList } from "./data"
 import './App.css'
 
-function App() {
-  return (
-    <>
-      <div>
 
-      </div>
-      
-    </>
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  return (
+    
+      <div>{
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+
+        }</div> 
+    
   )
 }
 
