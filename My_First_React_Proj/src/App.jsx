4da69cb0f266bae232de90b1b36fff0dec470988
jonathan296 <jonathanalvarenga296@gmail.com>
2023-10-03 00:52:57 -0700

    All done

--- My_First_React_Proj/src/App.jsx
+++ My_First_React_Proj/src/App.jsx
@@ -5,15 +5,34 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  
+  
   return (
     
-      <div>{
+      <div>
+        
+        <h1>Click on a puppy!</h1>
+        {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })}
+        
+        
+        <div className="featPuppy">{featPupId && (
+        <div>
+          <h1>Featured Puppy:</h1>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</div>
 
-        }</div> 
+      </div> 
+      
     
   )
 }
